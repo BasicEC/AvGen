ec0fb9bd1ea363e8e1d2c193a54937e01d3313d6
BasicEC <zak.vitaly@ya.ru>
2020-07-22 01:38:29 +0300

    write example in Console proj

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 using AvGen;
 
@@ -8,13 +9,54 @@ namespace ConsoleTest
     {
         public static void Main(string[] args)
         {
-            const string source = "basicec";
-            var v = new VerticalSymmetryAvGenerator(Color.WhiteSmoke, 600, 10, 50);
-            var c = new CentralSymmetryAvGenerator(Color.WhiteSmoke, 600, 16, 50);
-            var imageV = v.Generate(source);
-            var imageC = c.Generate(source);
-            imageV.Save($"{source}_V.png", ImageFormat.Png);
-            imageC.Save($"{source}_C.png", ImageFormat.Png);
+            const string first = "gallowsCalibrator";
+            const string second = "carcinoGeneticist";
+
+            var generator = new VerticalSymmetryAvGenerator(Color.WhiteSmoke, 600, 10, 50);
+
+            var fTemplate = generator.GenerateTemplate(first);
+            var sTemplate = generator.GenerateTemplate(second);
+            var fImage = generator.CreateBitmap(fTemplate);
+            var sImage = generator.CreateBitmap(sTemplate);
+
+            fImage.Save($"{first}.png", ImageFormat.Png);
+            sImage.Save($"{second}.png", ImageFormat.Png);
+
+            var colorDistance = Distance(fTemplate.Color, sTemplate.Color);
+            var colorPercent = colorDistance / Math.Sqrt(255 * 255 * 3) * 100;
+            var templateDistance = Distance(fTemplate.Template, sTemplate.Template);
+            var templatePercent = templateDistance / fTemplate.Template.Length * 100;
+
+            Console.WriteLine($"First Value:\t{first};\tColor: #{fTemplate.Color.R:X2}{fTemplate.Color.G:X2}{fTemplate.Color.B:X2};\n" +
+                              $"Second Value:\t{second};\tColor: #{sTemplate.Color.R:X2}{sTemplate.Color.G:X2}{sTemplate.Color.B:X2};\n\n" +
+                              $"Max Color Distance:\t{Math.Sqrt(255 * 255 * 3):F2};\n" +
+                              $"Color Distance:\t\t{colorDistance:F2};\n" +
+                              $"Color Match:\t\t{100 - colorPercent:F2}%;\n\n" +
+                              $"Max Template Distance:\t{fTemplate.Template.Length};\n" +
+                              $"Template Distance:\t{templateDistance};\n" +
+                              $"Template Match:\t\t{100 - templatePercent:F2}%;\n");
+        }
+
+        public static double Distance(Color first, Color second)
+        {
+            var r = first.R - second.R;
+            var g = first.G - second.G;
+            var b = first.B - second.B;
+            return Math.Sqrt((r * r) + (g * g) + (b * b));
+        }
+
+        public static double Distance(bool[,] first, bool[,] second)
+        {
+            var result = 0d;
+            for (var i = 0; i < first.GetLength(0); i++)
+            {
+                for (var j = 0; j < first.GetLength(1); j++)
+                {
+                    result += first[i, j] == second[i, j] ? 0 : 1;
+                }
+            }
+
+            return result;
         }
     }
 }
